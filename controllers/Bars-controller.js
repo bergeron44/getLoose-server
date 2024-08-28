fc66e1b385b3a30311508c0a2858dfafed0fd973
ron berger <ronberger40@gmail.com>
2024-08-28 14:00:43 +0300

    last update to bar controler

--- controllers/Bars-controller.js
+++ controllers/Bars-controller.js
@@ -8,8 +8,12 @@ const {
     addBar,
     updateBar,
     updateBarById,
+    addPackage // Import addPackage if necessary
 } = require('../services/Bars-services');
+
 const serverResponse = require('../utils/serverResponse');
+const Bars = require('../models/Bars'); // Ensure Bars model is imported
+const Packages = require('../models/Packages'); // Ensure Packages model is imported
 
 // Controller to get a bar by its name
 const getBarByNameCont = async (req, res) => {
@@ -59,15 +63,34 @@ const deleteBarByNameCont = async (req, res) => {
 // Controller to create a new bar
 const createBarCont = async (req, res) => {
     try {
-        const { barName, location, capacity } = req.body;
+        const { barName, location, capacity, packages } = req.body; // Expecting packages in the request body
+
         if (!barName || !location || capacity == null) {
             return serverResponse(res, 400, { message: "Missing required information" });
         }
+
         const existingBar = await getBar(barName);
         if (existingBar) {
             return serverResponse(res, 400, { message: "Bar already exists" });
         }
-        const newBar = await addBar(req.body);
+
+        // Create the new bar
+        const newBar = new Bars({ barName, location, capacity });
+        await newBar.save();
+
+        // Create and associate packages if provided
+        if (packages && packages.length > 0) {
+            const createdPackages = await Promise.all(packages.map(async (pkg) => {
+                const newPackage = new Packages(pkg);
+                await newPackage.save();
+                return newPackage._id; // Return the ID of the created package
+            }));
+
+            // Update the bar with the created packages
+            newBar.barPackages = createdPackages;
+            await newBar.save();
+        }
+
         return serverResponse(res, 201, newBar);
     } catch (e) {
         console.error(e);
@@ -79,24 +102,41 @@ const createBarCont = async (req, res) => {
 const editBarCont = async (req, res) => {
     try {
         let bar;
-        
+
         // Check if barName is provided in the params
         if (req.params.barName) {
             bar = await getBar(req.params.barName);
         }
-        
+
         // Check if barId is provided in the params
         if (req.params.barId) {
             bar = await getBarById(req.params.barId);
         }
-        
+
         // If no bar found with the given name or ID
         if (!bar) {
             return serverResponse(res, 404, { message: "No bar found with the given identifier" });
         }
-        
+
         // Update the bar
-        const updatedBar = await updateBar(bar.barName, req.body); // Assuming `updateBar` still uses barName as identifier
+        const updatedBar = await updateBar(bar.barName, req.body);
+
+        // Update packages if provided in the request body
+        if (req.body.packages) {
+            const { packages } = req.body;
+
+            // Create and associate new packages
+            const createdPackages = await Promise.all(packages.map(async (pkg) => {
+                const newPackage = new Packages(pkg);
+                await newPackage.save();
+                return newPackage._id;
+            }));
+
+            // Update the bar with the new packages
+            updatedBar.barPackages = createdPackages;
+            await updatedBar.save();
+        }
+
         return serverResponse(res, 200, updatedBar);
     } catch (e) {
         console.error(e);
@@ -118,6 +158,8 @@ const getAllBarsNamesCont = async (req, res) => {
         return serverResponse(res, 500, { message: 'Internal error occurred while trying to get all bar names' });
     }
 };
+
+// Controller to get all bars
 const getAllBarsCont = async (req, res) => {
     try {
         const allBars = await getAllBars();
@@ -130,7 +172,6 @@ const getAllBarsCont = async (req, res) => {
         return serverResponse(res, 500, { message: 'Internal error occurred while trying to get all bars' });
     }
 };
-
 
 module.exports = {
     getBarByNameCont,
